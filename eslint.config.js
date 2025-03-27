e7b64979efaa34deae0b2a3cc48c031691a8a45a
Aleksi Alhola <aleksi.alhola@gmail.com>
2025-03-27 19:56:06 +0200

    export recommended rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,23 @@
 import { errorRules, omittedFromTests, plugins, warningRules } from "./index.js";
 import { defineConfig } from "eslint/config";
 
+export const recommended = {
+  files: ["**/*.{js,ts,jsx,tsx}"],
+  name: "@fbgfi/eslint-config",
+  plugins,
+  rules: {
+    "@fbgfi/no-global-arrow-function": "error",
+    "@fbgfi/no-mutating-input-objects": "warn",
+    "@fbgfi/no-reassign-objects": "warn",
+    "@fbgfi/no-spread-in-reduce": "error",
+    ...errorRules,
+    ...warningRules,
+    ...omittedFromTests
+  }
+};
+
 export default defineConfig([
-  {
-    files: ["**/*.{js,ts,jsx,tsx}"],
-    name: "@fbgfi/eslint-config",
-    plugins,
-    rules: {
-      "@fbgfi/no-global-arrow-function": "error",
-      "@fbgfi/no-mutating-input-objects": "warn",
-      "@fbgfi/no-reassign-objects": "warn",
-      "@fbgfi/no-spread-in-reduce": "error",
-      ...errorRules,
-      ...warningRules,
-      ...omittedFromTests
-    }
-  },
+  recommended,
   {
     files: ["**/*.{test,spec}.{js,ts,jsx,tsx}"],
     rules: Object.keys(omittedFromTests).reduce((rules, key) => {
